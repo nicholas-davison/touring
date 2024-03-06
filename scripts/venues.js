b24c711982a3e4422cac191d967631e9d1591be7
Nicholas Davison <nick.a.davison@gmail.com>
2024-03-06 17:07:32 -0600

    added click event to venues

--- scripts/venues.js
+++ scripts/venues.js
@@ -1,6 +1,8 @@
 //import venues data from database
-import { getVenues, getBookings } from "./database.js"
+import { getVenues, getBookings, getBands } from "./database.js"
 const allVenues = getVenues()
+const allBookings = getBookings()
+const allBands = getBands()
 //build an html string for each entry, represented as list items, and export as a function. include data attributes for event listener function.
 
 export const displayVenues = () => {
@@ -18,7 +20,20 @@ document.addEventListener(
     (clickEvent) => {
         const itemClicked = clickEvent.target
         if (itemClicked.dataset.type === "venue") {
-            
+            let bandIDs = []
+            for (const booking of allBookings) {
+                if (parseInt(itemClicked.dataset.id) === booking.venueID) {
+                    bandIDs.push(booking.bandID)
+                }
+            } 
+            let bandNames = []
+            for (const band of bandIDs) {
+                for (const bandObj of allBands) {
+                    if (band === bandObj.id) {
+                        bandNames.push(bandObj.bandName)
+                    }
+                }
+            } window.alert(`${bandNames} are playing at ${itemClicked.dataset.name}`)
         }
     }
     
